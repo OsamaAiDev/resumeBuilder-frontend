817d8b4bb7aa3bf52169ca41f0c1337586902e11
OsamaAiDev <149548703+OsamaAiDev@users.noreply.github.com>
2025-09-03 16:21:12 +0500

    updated index js

--- src/utils/apiPaths.js
+++ src/utils/apiPaths.js
@@ -1,7 +1,30 @@
-export const BASE_URL = "http://localhost:4000/";
+// export const BASE_URL = "http://localhost:4000/";
+
+// // routes used for frontend
+
+// export const API_PATHS = {
+//   AUTH: {
+//     REGISTER: "/api/auth/register",
+//     LOGIN: "/api/auth/login",
+//     GET_PROFILE: "/api/auth/profile",
+//   },
+//   RESUME: {
+//     CREATE: "/api/resume",
+//     GET_ALL: "/api/resume",
+//     GET_BY_ID: (id) => `/api/resume/${id}`,
+//     UPDATE: (id) => `/api/resume/${id}`,
+//     DELETE: (id) => `/api/resume/${id}`,
+//     UPLOAD_IMAGES: (id) => `/api/resume/${id}/upload-images`,
+//   },
+//   image: {
+//     UPLOAD_IMAGE: "api/auth/upload-image",
+//   },
+// };
+// new code
+export const BASE_URL =
+  process.env.REACT_APP_BASE_URL || "http://localhost:4000";
 
 // routes used for frontend
-
 export const API_PATHS = {
   AUTH: {
     REGISTER: "/api/auth/register",
@@ -17,6 +40,6 @@ export const API_PATHS = {
     UPLOAD_IMAGES: (id) => `/api/resume/${id}/upload-images`,
   },
   image: {
-    UPLOAD_IMAGE: "api/auth/upload-image",
+    UPLOAD_IMAGE: "/api/auth/upload-image",
   },
 };
