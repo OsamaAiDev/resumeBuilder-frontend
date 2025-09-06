4b2bb969a912176b058ff0a3e1a52a3c37448238
OsamaAiDev <149548703+OsamaAiDev@users.noreply.github.com>
2025-09-06 11:35:39 +0500

    updated api paths js

--- src/utils/apiPaths.js
+++ src/utils/apiPaths.js
@@ -1,28 +1,9 @@
-// export const BASE_URL = "http://localhost:4000/";
-
-// // routes used for frontend
-
-// export const API_PATHS = {
-//   AUTH: {
-//     REGISTER: "/api/auth/register",
-//     LOGIN: "/api/auth/login",
-//     GET_PROFILE: "/api/auth/profile",
-//   },
-//   RESUME: {
-//     CREATE: "/api/resume",
-//     GET_ALL: "/api/resume",
-//     GET_BY_ID: (id) => `/api/resume/${id}`,
-//     UPDATE: (id) => `/api/resume/${id}`,
-//     DELETE: (id) => `/api/resume/${id}`,
-//     UPLOAD_IMAGES: (id) => `/api/resume/${id}/upload-images`,
-//   },
-//   image: {
-//     UPLOAD_IMAGE: "api/auth/upload-image",
-//   },
-// };
-// new code
 export const BASE_URL =
-  process.env.REACT_APP_BASE_URL || "http://localhost:4000";
+  process.env.REACT_APP_BASE_URL ||
+  (process.env.NODE_ENV === "production"
+    ? "https://resume-builder-backend-flame.vercel.app"
+    : "http://localhost:4000");
+console.log("ENV variable", REACT_APP_BASE_URL);
 
 // routes used for frontend
 export const API_PATHS = {
